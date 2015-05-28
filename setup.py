d15e4d57d71ab8e74da69e034761a349ac1ff733
Punit Dhoot <punitdhoot1@gmail.com>
2015-05-28 15:33:43 +0530

    Added setup.py and setup.cfg files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'Compare-Me',
+  packages = ['Compare-Me'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Peter Downs',
+  author_email = 'punitdhoot1@gmail.com',
+  url = 'https://github.com/pdhoot/Compare-Me', # use the URL to the github repo
+  download_url = 'https://github.com/pdhoot/Compare-Me/tarball/v1', # I'll explain this in a second
+  keywords = ['compare' , 'users' , 'spoj' ,'codechef' , 'github'], # arbitrary keywords
+  classifiers = [],
+)
